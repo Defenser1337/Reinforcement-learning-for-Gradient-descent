733c72e1c3d798b309db5a1d3f12b1cdc0d1325d
DefenserPC <lolik1920@yandex.ru>
2026-03-21 14:05:22 +0500

    Updated 2dim model train

--- src/rl_model_training/convex_model/2d_convex_model_train.py
+++ src/rl_model_training/convex_model/2d_convex_model_train.py
@@ -4,11 +4,18 @@ from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.vec_env import VecNormalize
 import src.gymnasium_envs.convex_optimization_env
 
+log_dir = "logs"
+dim = 2
+
+config = {
+    2 : {"timesteps": 10_000, "n_envs": 14, "batch_size": 1024, "policy_kwargs": {}}
+}
+
 vec_env = make_vec_env(
     "convex_optimization_env/ConvexOptimization-v0", 
-    n_envs=14,
+    n_envs=config[dim]["n_envs"],
     env_kwargs={
-        "in_features": 2
+        "in_features": dim
     })
 
 vec_env = VecNormalize(
@@ -17,12 +24,14 @@ vec_env = VecNormalize(
 )
 
 model = PPO(
-    "MultiInputPolicy", 
-    vec_env, 
-    verbose=1
+    "MultiInputPolicy",
+    vec_env,
+    verbose=1,  
+    tensorboard_log=f"{log_dir}/{dim}d/",
+    policy_kwargs=config[dim]["policy_kwargs"]
 )
 
-model.learn(total_timesteps=250000)
+model.learn(total_timesteps=config[dim]["timesteps"])
 
 model.save("2d_convex_optimization")
 vec_env.save("2d_convex_optimization_vec_normalize_stats.pkl")
