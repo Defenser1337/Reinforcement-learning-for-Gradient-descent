e9a81a29e597312d95d7e2bbfac11b992bf7af49
DefenserPC <lolik1920@yandex.ru>
2026-03-27 14:30:18 +0500

    Updated file name

--- src/gymnasium_envs/nn_optimization_env/__init__.py
+++ src/gymnasium_envs/nn_optimization_env/__init__.py
@@ -1,7 +1,7 @@
 from gymnasium.envs.registration import register
 
 register(
-    id="convex_optimization_env/NeuralNetworkOptimization-v0",
+    id="nn_optimization_env/NeuralNetworkOptimization-v0",
     entry_point="src.gymnasium_envs.nn_optimization_env.envs.nn_optimization:NeuralNetworkOptimization",
 )
 
