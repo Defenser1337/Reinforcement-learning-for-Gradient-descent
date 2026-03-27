0f14e41e14e38f73f44e077ad26feffb021bd919
DefenserPC <lolik1920@yandex.ru>
2026-03-27 14:37:56 +0500

    Updated file name and file tree

---
+++ src/gymnasium_envs/env_check.py
@@ -0,0 +1,11 @@
+import gymnasium
+from stable_baselines3.common.env_checker import check_env
+
+import src.gymnasium_envs.convex_optimization_env
+import src.gymnasium_envs.nn_optimization_env
+
+env1 = gymnasium.make("convex_optimization_env/ConvexOptimization-v0")
+env2 = gymnasium.make("nn_optimization_env/NeuralNetworkOptimization-v0", dataset_name="MNIST")
+
+check_env(env1)
+check_env(env2)
--- src/utils/sb3_env_check.py
+++
@@ -1,8 +0,0 @@
-import gymnasium
-import src.gymnasium_envs.convex_optimization_env
-
-from stable_baselines3.common.env_checker import check_env
-
-env = gymnasium.make("convex_optimization_env/ConvexOptimization-v0")
-
-check_env(env)
