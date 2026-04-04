a3684e22f660b1cb7519485b94a9e2be6c8e79fe
DefenserPC <lolik1920@yandex.ru>
2026-04-04 16:53:58 +0500

    Added Convex Optimization V1 env

--- src/gymnasium_envs/convex_optimization_env/__init__.py
+++ src/gymnasium_envs/convex_optimization_env/__init__.py
@@ -5,6 +5,12 @@ register(
     entry_point="src.gymnasium_envs.convex_optimization_env.envs.convex_optimization_v0:ConvexOptimizationV0",
 )
 
-from src.gymnasium_envs.convex_optimization_env.envs.convex_optimization_v0 import ConvexOptimizationV0
+register(
+    id="convex_optimization_env/ConvexOptimization-v1",
+    entry_point="src.gymnasium_envs.convex_optimization_env.envs.convex_optimization_v1:ConvexOptimizationV1",
+)
 
-__all__ = ["ConvexOptimizationV0"]
+from src.gymnasium_envs.convex_optimization_env.envs.convex_optimization_v0 import ConvexOptimizationV0
+from src.gymnasium_envs.convex_optimization_env.envs.convex_optimization_v1 import ConvexOptimizationV1
+
+__all__ = ["ConvexOptimizationV0", "ConvexOptimizationV1"]
--- src/gymnasium_envs/convex_optimization_env/envs/__init__.py
+++ src/gymnasium_envs/convex_optimization_env/envs/__init__.py
@@ -1,3 +1,4 @@
 from src.gymnasium_envs.convex_optimization_env.envs.convex_optimization_v0 import ConvexOptimizationV0
+from src.gymnasium_envs.convex_optimization_env.envs.convex_optimization_v1 import ConvexOptimizationV1
 
-__all__ = ["ConvexOptimizationV0"]
+__all__ = ["ConvexOptimizationV0", "ConvexOptimizationV1"]
