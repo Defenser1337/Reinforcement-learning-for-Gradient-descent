174095c6dfa57135bef87065bce5926abb0eac14
DefenserPC <lolik1920@yandex.ru>
2026-05-25 14:31:27 +0500

    Added None proccesing to get_rng

--- src/utils/prng.py
+++ src/utils/prng.py
@@ -1,7 +1,11 @@
 import numpy as np
 import hashlib
+from typing import Optional
 
-def get_rng(seed, location_name):
+def get_rng(seed : Optional[int], location_name):
+    if seed is None:
+        return np.random.default_rng()
+
     local_seed = int(hashlib.md5(location_name.encode()).hexdigest(), 16) & 0xFFFFFFFF
     combined_seed = (local_seed + int(seed)) & 0xFFFFFFFF
     return np.random.default_rng(combined_seed)
